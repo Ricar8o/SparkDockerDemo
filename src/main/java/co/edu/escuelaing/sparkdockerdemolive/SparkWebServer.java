1832355deffc698413f6a0f86f7b09c2e4a8abd0
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-09-29 10:21:28 -0500

    Subir

---
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -0,0 +1,28 @@
+package co.edu.escuelaing.sparkdockerdemolive;
+ 
+import static spark.Spark.*;
+ 
+ 
+/**
+
+  * A simple web server using Sparkweb
+
+  * @author dnielben
+
+  */
+
+ public class SparkWebServer {
+     public static void main(String... args){
+           port(getPort());
+           get("hello", (req,res) -> "Hello Docker! :)");
+
+     }
+ 
+    private static int getPort() {
+         if (System.getenv("PORT") != null) {
+             return Integer.parseInt(System.getenv("PORT"));
+         }
+         return 4567;
+     }
+
+ }
